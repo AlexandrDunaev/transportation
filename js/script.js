b7f6c9a23305b34d08570f9344cbe67fe84d79ec
Alexandr Dunaev <ghost74rus@gmail.com>
2022-06-22 14:02:59 +0500

    landing page commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+   $('.bxslider').bxSlider({
+      autoStart: true,
+      speed: 800,
+      mode: 'fade',
+      auto: true,
+      pause: 4000,
+      touchEnabled: false
+   });
+});
