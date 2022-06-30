24f90eee88afbbbf3b0b215c06728831c8179595
Alexandr Dunaev <ghost74rus@gmail.com>
2022-06-30 23:31:34 +0500

    end commit

--- js/script.js
+++ js/script.js
@@ -15,3 +15,25 @@ window.addEventListener('DOMContentLoaded', function () {
          document.querySelector('#burger-menu').classList.toggle('is-active')
    })
 });
+
+$(function () {
+   $("#accordion").accordion({
+      heightStyle: "content"
+   });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+   document.querySelectorAll('.tabs-nav__btn').forEach(function (tabsBtn) {
+      tabsBtn.addEventListener('click', function (event) {
+         const path = event.currentTarget.dataset.path
+
+         document.querySelectorAll(".tabs-item").forEach(function (tabContent) {
+            tabContent.classList.remove("tabs-item--active")
+         })
+
+         document.querySelector(`[data-target="${path}"]`).classList.add('tabs-item--active')
+
+      })
+   })
+});
+
